a1ff19d704ce92de30d2327f8192ac342a5698e6
Victor Azevedo <victorazesc@hotmail.com>
2023-10-24 21:47:51 -0300

    refactor: input header page

--- application/views/components/pageHeader.php
+++ application/views/components/pageHeader.php
@@ -6,9 +6,11 @@
   </h4>
 
   <div style="display: flex; justify-content: space-between">
-    <div class="form-group">
-      <input type="text" value="<?= isset($_GET['search']) ? $_GET['search'] : '' ?>" name="termo"
-        oninput="_onInputChange(event)" class="form-control" placeholder="<?= $pageData['searchPlaceholder'] ?>">
+    <div class="form-group" style="position: relative;">
+      <i class="ri-search-line" style="position: absolute;bottom: 6px;left: 6px;"></i>
+      <input style="padding-left: 32px" type="text" value="<?= isset($_GET['search']) ? $_GET['search'] : '' ?>"
+        name="termo" oninput="_onInputChange(event)" class="form-control"
+        placeholder="<?= $pageData['searchPlaceholder'] ?>">
     </div>
     <a class="btn btn-primary" href="<?php echo base_url() . $this->uri->segment(1) ?>/adicionar"><i class="fas fa-plus"
         style="font-size: 0.75rem;"></i> Adicionar </a>
